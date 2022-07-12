52b72530f7dfee3e40dfd9eb8cc50b626bc807f4
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-07-12 15:50:59 -0400

    tmp

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(
     name="beaker",
     version="0.0.1dev",
-    packages=[
-        "beaker",
-    ],
+    packages=find_packages(),
     license="MIT",
     long_description=open("README.md").read(),
 )
