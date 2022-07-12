06e78819451c52b407c14fc8d078a0944c56a546
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-07-12 14:29:39 -0400

    adding sandbox

--- beaker/decorators_test.py
+++ beaker/decorators_test.py
@@ -1,7 +1,9 @@
 import pytest
 import pyteal as pt
 
-from .decorators import handler, get_handler_config, Authorize, Bare
+from .application import Application
+
+from .decorators import internal, handler, get_handler_config, Authorize, Bare
 
 options = pt.CompileOptions(mode=pt.Mode.Application, version=pt.MAX_TEAL_VERSION)
 
@@ -211,3 +213,21 @@ def test_bare():
 
     hc = get_handler_config(impl)
     assert hc.bare_method.clear_state.action.subroutine.implementation == impl
+
+
+#def test_internal():
+#
+#    class Internal(Application):
+#
+#        @internal(pt.TealType.uint64)
+#        def internal_meth(self):
+#            return pt.Int(1)
+#
+#    print()
+#    i = Internal()
+#    print(Internal.internal_meth)
+#    print(i.internal_meth)
+#    hc1 = get_handler_config(Internal.internal_meth)
+#    hc2 = get_handler_config(i.internal_meth)
+#    print(hc1)
+#    print(hc2)
