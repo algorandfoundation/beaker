c449b53b7e8d9d2f005a4b80feda62771ac4b32f
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-07-14 18:49:04 -0400

    adding account state example

---
+++ examples/account_state/contract.py
@@ -0,0 +1,22 @@
+from typing import Final
+from beaker import *
+from pyteal import *
+
+
+class Dope(Application):
+
+    stuff: Final[DynamicLocalStateValue] = DynamicLocalStateValue(
+        stack_type=TealType.bytes, max_keys=16, key_gen=Subroutine(TealType.bytes)(lambda i: Suffix(Itob(i), Int(7))),
+    )
+
+    @Bare.opt_in
+    def optin(self):
+        return self.initialize_account_state(Txn.sender())
+        
+    @handler
+    def doit(self, k: abi.Uint64, v: abi.String):
+        return self.stuff(k.get()).set(Txn.sender(), v.get())
+
+if __name__ == "__main__":
+    d = Dope()
+    print(d.approval_program)
---
+++ examples/account_state/main.py
@@ -0,0 +1,27 @@
+from algosdk.atomic_transaction_composer import AccountTransactionSigner
+from contract import Dope
+from beaker.client import ApplicationClient
+from beaker.sandbox import get_client, get_accounts
+
+accts = get_accounts()
+
+acct,sk = accts[0]
+signer = AccountTransactionSigner(sk)
+
+client = get_client()
+
+app = Dope()
+app_client = ApplicationClient(client, app, signer=signer)
+app_id, app_address, transaction_id = app_client.create()
+print(f"DEPLOYED: App ID: {app_id} Address: {app_address} Transaction ID: {transaction_id}")
+
+print(f"Opting in")
+result = app_client.opt_in()
+print(f"Result: {result}")
+
+print("Setting first item")
+result = app_client.call(app.doit, k=0, v="First item")
+print(f"Result: {result.return_value}")
+
+ls = client.account_application_info(acct, app_client.app_id)
+print(ls['app-local-state'])
