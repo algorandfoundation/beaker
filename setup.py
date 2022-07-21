ae8d01f1de0a79798c2a7ec8177ca572b57b0a5b
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-07-21 15:11:20 -0400

    Add py.typed

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ from setuptools import find_packages
 setup(
     name="beaker",
     version="0.0.1dev",
+    python_requires=">=3.10",
     packages=find_packages(),
     license="MIT",
     long_description=open("README.md").read(),
+    package_data={"beaker":["py.typed"]}
     install_requires=[
         "pyteal @ git+https://github.com/algorand/pyteal@feature/abi#egg=pyteal"
     ],
