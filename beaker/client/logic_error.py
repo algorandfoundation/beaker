0c1a6b8f020c07f57cd6f65afa8ac7d953f9c8bf
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-07-25 12:29:56 -0400

    verbiage on error pointer

--- beaker/client/logic_error.py
+++ beaker/client/logic_error.py
@@ -14,7 +14,7 @@ def parse_logic_error(error_str: str) -> tuple[str, int]:
 
 def tiny_trace(approval_program: str, line_no: int, lines: int) -> str:
     approval_lines = approval_program.split("\n")
-    approval_lines[line_no] += " <-- This one right here was where we failed"
+    approval_lines[line_no] += "\t\t<-- Error"
     lines_before = max(0, line_no - lines)
     lines_after = min(len(approval_lines), line_no + lines)
     return "\n\t".join(approval_lines[lines_before:lines_after])
@@ -33,9 +33,10 @@ class LogicException(Exception):
         self.app = app
         self.approval_map = approval_map
         self.clear_map = clear_map
+        # TODO: check if it was the clear or approval program
+        self.txid, self.pc = parse_logic_error(self.logic_error_str)
 
     def __str__(self):
-        txid, pc = parse_logic_error(self.logic_error_str)
-        line_no = self.approval_map.get_line_for_pc(pc)
+        line_no = self.approval_map.get_line_for_pc(self.pc)
         trace = tiny_trace(self.app.approval_program, line_no, 5)
-        return f"Txn({txid}) had logic error at pc {pc} and source line {line_no}: \n\t{trace}"
+        return f"Txn({self.txid}) had logic error at pc {self.pc} and source line {line_no}: \n\n\t{trace}"
