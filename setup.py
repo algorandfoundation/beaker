29cc9519ed69c2bf1cf50bce07e61c0d2469e65d
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-07-25 13:18:31 -0400

    update reqs and setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     license="MIT",
     long_description=open("README.md").read(),
     package_data={"beaker": ["py.typed"]},
-    install_requires=[
-        "pyteal @ git+https://github.com/algorand/pyteal@master#egg=pyteal"
-    ],
+    install_requires=["pyteal>=0.14.0", "py-algorand-sdk>=1.16.0"]
 )
+
