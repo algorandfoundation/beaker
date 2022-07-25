89c92c5cd367d84f90833c82539689f4ad6e0555
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-07-25 13:23:07 -0400

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="beaker",
-    version="0.0.1-3-dev",
+    version="0.0.1-4-dev",
     python_requires=">=3.10",
     packages=find_packages(),
     license="MIT",
