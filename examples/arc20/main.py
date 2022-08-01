b3c63969ef83c97e1821a23eaa2673e290098d7e
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-01 14:50:31 -0400

    fmt

--- examples/arc20/main.py
+++ examples/arc20/main.py
@@ -1,5 +1,13 @@
-from algosdk.future.transaction import PaymentTxn, wait_for_confirmation, AssetOptInTxn, OnComplete
-from algosdk.atomic_transaction_composer import AccountTransactionSigner, TransactionWithSigner
+from algosdk.future.transaction import (
+    PaymentTxn,
+    wait_for_confirmation,
+    AssetOptInTxn,
+    OnComplete,
+)
+from algosdk.atomic_transaction_composer import (
+    AccountTransactionSigner,
+    TransactionWithSigner,
+)
 from beaker.contracts.arcs import ARC20
 from beaker.sandbox import get_accounts, get_client
 from beaker.client import ApplicationClient
@@ -14,16 +22,15 @@ def demo():
     signer = AccountTransactionSigner(sk)
 
     app = ARC20()
-    #with open("approval.teal", "w") as f:
+    # with open("approval.teal", "w") as f:
     #    f.write(app.approval_program)
-
 
     app_client = ApplicationClient(algod_client, app=app, signer=signer)
 
-    #_, sm = app_client.compile_approval(True)
-    #print(sm.line_to_pc)
-    #lines = app.approval_program.split("\n")
-    #with open("approval.mapped.teal", "w") as f:
+    # _, sm = app_client.compile_approval(True)
+    # print(sm.line_to_pc)
+    # lines = app.approval_program.split("\n")
+    # with open("approval.mapped.teal", "w") as f:
     #    annotated_lines: list[str] = []
     #    for idx, line in enumerate(lines):
     #        annotated_lines.append(line + f" // {sm.get_pcs_for_line(idx)}")
@@ -33,14 +40,15 @@ def demo():
     print(f"Created app: {app_id} with address {app_addr}")
 
     sp = algod_client.suggested_params()
-    txid = algod_client.send_transaction(PaymentTxn(addr, sp, app_addr, int(1e6)).sign(sk))
+    txid = algod_client.send_transaction(
+        PaymentTxn(addr, sp, app_addr, int(1e6)).sign(sk)
+    )
     wait_for_confirmation(algod_client=algod_client, txid=txid, wait_rounds=4)
-
 
     sp = algod_client.suggested_params()
     sp.flat_fee = True
     sp.fee = sp.min_fee * 2
-    
+
     result = app_client.call(
         app.asset_create,
         suggested_params=sp,
@@ -57,7 +65,7 @@ def demo():
         reserve_addr=addr,
     )
 
-    smart_asa_id=result.return_value
+    smart_asa_id = result.return_value
 
     print(f"Created asset with asset id: {smart_asa_id}")
 
@@ -89,7 +97,7 @@ def demo():
             opt_in_txn=TransactionWithSigner(
                 txn=AssetOptInTxn(addr, sp, smart_asa_id),
                 signer=signer,
-            )
+            ),
         )
     except Exception as e:
         print(app_client.wrap_approval_exception(e, 100))
@@ -97,6 +105,5 @@ def demo():
 
 
 if __name__ == "__main__":
-
 
     demo()
