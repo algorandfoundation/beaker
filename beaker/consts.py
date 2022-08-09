3a38c00039ce0da72d09e986881e51e9b6796a8d
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-09 14:30:54 -0400

    docstrings

--- beaker/consts.py
+++ beaker/consts.py
@@ -1,18 +1,19 @@
 from typing import Final, Callable
 from pyteal import Int, Expr
 
-#: Used these for compile time consts like `payment_amt = Int(10 * algo)`
 #: number of microalgos in 1 Algo
 algo: Final[int] = int(1e6)
 #: number of microalgos in 1 MilliAlgo
 milli_algo: Final[int] = int(1e3)
 
-#: Used for runtime algo calculations `Txn.fee()==MilliAlgo`
+#: Used for runtime algo calculations `Txn.amount()==Algo`
 Algo: Final[Int] = Int(algo)
+#: Used for runtime algo calculations `Txn.fee()==MilliAlgo`
 MilliAlgo: Final[Int] = Int(milli_algo)
 
 #: Used for shorthand for Int(10*algo) like Algos(10)
 Algos: Callable[..., Expr] = lambda v: Int(int(v * algo))
+#: Used for shorthand for Int(10*milli_algo) like MilliAlgos(10)
 MilliAlgos: Callable[..., Expr] = lambda v: Int(int(v * milli_algo))
 
 #: Max number of inner transactions that may be called
@@ -22,7 +23,9 @@ APP_CALL_BUDGET = 700
 #: Max possible opcode budget
 MAX_OPS = MAX_INNERS * APP_CALL_BUDGET
 
+#: Single app call budget
 AppCallBudget = Int(APP_CALL_BUDGET)
+#: Max app call budget possible
 MaxOps = Int(MAX_OPS)
 
 
