692321c90fda40aaf0366a4618890f0fc59cac69
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-10 14:33:06 -0400

    remove accumulate

--- beaker/lib/iter/iter.py
+++ beaker/lib/iter/iter.py
@@ -1,38 +1,4 @@
-from typing import List
-
-from pyteal import BinaryExpr, Expr, For, Int, Op, ScratchVar, Seq, Subroutine, TealType
-
-
-def accumulate(vals: List[Expr], op: Op) -> Expr:
-    """accumulate provides a convenient way to efficiently combine elements in a list.
-
-    Pairs elements of the vals list recursively to save on ops and reduce the need for intermediate scratch var storing/loading
-
-    Typical Usage Example:
-        accumulate([Int(1), Int(2), Int(3), Int(4)], Op.Add) # comes out to (1+2)+(3+4) == 10
-
-    Args:
-        vals: A List of Expr objects to evaluate, these should evaluate to a Bytes or Int
-        op: A teal operation to perform on the list
-
-    Returns:
-        A single expression representing the accumulated value after applying the op for all elements
-
-    """
-    ops: List[Expr] = []
-    for n in range(0, len(vals) - 1, 2):
-        ops.append(
-            BinaryExpr(op, TealType.uint64, TealType.uint64, vals[n], vals[n + 1])
-        )
-
-    # If its an odd number, we cant match it, just add the last one
-    if len(vals) % 2 == 1:
-        ops.append(vals[-1])
-
-    if len(ops) > 1:
-        return accumulate(ops, op)
-
-    return Seq(ops)
+from pyteal import Expr, For, Int, ScratchVar, Subroutine, TealType
 
 
 def iterate(sub: Expr, n: Int, i: ScratchVar = ScratchVar()) -> Expr:
