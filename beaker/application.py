96bc905f18c2e38f18fb115414f7fc2b9e163944
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-17 10:56:35 -0400

    adding approval/clear program to app spec (#60)

--- beaker/application.py
+++ beaker/application.py
@@ -1,3 +1,4 @@
+import base64
 from inspect import getattr_static
 from typing import Final, Any, cast, Optional
 from algosdk.abi import Method
@@ -197,6 +198,10 @@ class Application:
         """returns a dictionary, helpful to provide to callers with information about the application specification"""
         return {
             "hints": {k: v.dictify() for k, v in self.hints.items() if not v.empty()},
+            "source":{
+                "approval":base64.b64encode(self.approval_program.encode()).decode("utf8"),
+                "clear":base64.b64encode(self.clear_program.encode()).decode("utf8"),
+            },
             "schema": {
                 "local": self.acct_state.dictify(),
                 "global": self.app_state.dictify(),
