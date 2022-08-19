892922d29d20ed4e8981464237b69d52df872377
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-19 08:29:47 -0400

    adding example to create template contract

--- examples/account-storage/main.py
+++ examples/account-storage/main.py
@@ -1,5 +1,5 @@
 from typing import cast
-from algosdk.future.transaction import PaymentTxn
+import algosdk.future.transaction as txns
 from algosdk.atomic_transaction_composer import *
 from pyteal import *
 from beaker import *
@@ -12,26 +12,18 @@ else:
 
 
 class DiskHungry(Application):
-
-    blob_space = DynamicAccountStateValue(TealType.bytes, max_keys=16)
+    stuff = AccountStateValue(TealType.bytes, default=Bytes("lol"))
 
     tmpl_acct = Precompile(KeySig(version=6))
 
-    @external
-    def add_account(self, nonce: abi.DynamicArray[abi.Byte], *, output: abi.Address):
-        return output.set(
-            self.tmpl_acct.template_address(Suffix(nonce.encode(), Int(2)))
-        )
-
-    @external
-    def populate_contract(
-        self, nonce: abi.DynamicArray[abi.Byte], *, output: abi.DynamicArray[abi.Byte]
-    ):
+    @external(method_config=MethodConfig(opt_in=CallConfig.CALL))
+    def add_account(self, nonce: abi.DynamicArray[abi.Byte]):
         return Seq(
-            (s := abi.String()).set(
-                self.tmpl_acct.populate_template_expr(Suffix(nonce.encode(), Int(2)))
+            Assert(
+                Txn.sender()
+                == self.tmpl_acct.template_address(Suffix(nonce.encode(), Int(2)))
             ),
-            output.decode(s.encode()),
+            self.initialize_account_state(),
         )
 
 
@@ -41,44 +33,43 @@ def demo():
     app_client = client.ApplicationClient(
         sandbox.get_algod_client(), DiskHungry(), signer=acct.signer
     )
-    # app_client.build()
-
     app_client.create()
 
-    atc = AtomicTransactionComposer()
-    sp = app_client.get_suggested_params()
-    sp.flat_fee = True
-    sp.fee = 2 * consts.milli_algo
-    atc.add_transaction(
-        TransactionWithSigner(
-            txn=PaymentTxn(acct.address, sp, acct.address, 0), signer=acct.signer
+    tmpl_acct = cast(DiskHungry, app_client.app).tmpl_acct
+    for x in range(10):
+        nonce = x.to_bytes(8, "big")
+
+        lsig_signer = tmpl_acct.template_signer(nonce)
+        lsig_client = app_client.prepare(signer=lsig_signer)
+
+        atc = AtomicTransactionComposer()
+
+        sp = app_client.get_suggested_params()
+        sp.flat_fee = True
+        sp.fee = 2 * consts.milli_algo
+        atc.add_transaction(
+            TransactionWithSigner(
+                txn=txns.PaymentTxn(
+                    acct.address, sp, lsig_signer.lsig.address(), consts.algo
+                ),
+                signer=acct.signer,
+            )
         )
-    )
 
-    # tmpl_vals = {"nonce": "dead", "inty": 123, "noncer": "beef", "interoo": 222}
+        sp = app_client.get_suggested_params()
+        sp.flat_fee = True
+        sp.fee = 0
+        lsig_client.add_method_call(
+            atc,
+            DiskHungry.add_account,
+            nonce=nonce,
+            suggested_params=sp,
+            on_complete=txns.OnComplete.OptInOC,
+        )
 
-    app_client.add_method_call(atc, DiskHungry.add_account, nonce=b"dead")
-    # app_client.add_method_call(atc, DiskHungry.populate_contract, nonce=b"dead")
+        atc.execute(app_client.client, 4)
 
-    result = atc.execute(app_client.client, 4)
-    print(result.abi_results[0].return_value)
-
-    dh = cast(DiskHungry, app_client.app)
-    ta = dh.tmpl_acct
-    # print(list(ta.populate_template(b"dead")))
-    print(ta.template_signer(b"dead").lsig.address())
-
-    print(app_client.app.approval_program)
-
-    # print(tmpl_signer.lsig.address())
-    # app_client.call(DiskHungry.add_account, "blah")
-
-    # bin, addr, map = app_client.compile(ta.teal(), True)
-    # ta.set_compiled(bin, addr, map)
-    # print(ta.__dict__)
-    # print(ta.template_address(Bytes("asdf")))
-
-    # app_client.build()
+        print(lsig_client.get_account_state())
 
 
 if __name__ == "__main__":
