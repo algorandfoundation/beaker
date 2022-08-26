b23cc97d4c4389f01f85815157909a4087d2ccb8
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-26 11:56:56 -0400

    tmp

--- beaker/decorators.py
+++ beaker/decorators.py
@@ -159,13 +159,16 @@ class HandlerConfig:
             mh.param_annotations = self.param_annotations
 
         if self.structs is not None:
-            mh.structs = {
-                arg_name: {
+            mh.structs = {}
+            for arg_name, model_spec in self.structs.items():
+                annos =  list(model_spec.__annotations__.items())
+                mh.structs[arg_name] = {
                     "name": str(model_spec.__name__),  # type: ignore[attr-defined]
-                    "elements": list(model_spec.__annotations__.keys()),
+                    "elements": [
+                        (name, str(abi.algosdk_from_annotation(typ.__args__[0])))
+                        for name, typ in annos
+                    ]
                 }
-                for arg_name, model_spec in self.structs.items()
-            }
 
         return mh
 
--- examples/structure/main.py
+++ examples/structure/main.py
@@ -111,9 +111,9 @@ def demo():
 
 
 if __name__ == "__main__":
-    demo()
+    #demo()
 
-    #import json
-    #s = Structer()    
-    #with open("struct-app.json", "w") as f:
-    #    f.write(json.dumps((s.application_spec())))
+    import json
+    s = Structer()    
+    with open("structer.json", "w") as f:
+        f.write(json.dumps((s.application_spec())))
