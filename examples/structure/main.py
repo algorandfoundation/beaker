e1ce0c41ff0f7df765b5e4d73fec804d7443fd1a
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-27 07:40:26 -0400

    re-enable demo

--- examples/structure/main.py
+++ examples/structure/main.py
@@ -111,9 +111,9 @@ def demo():
 
 
 if __name__ == "__main__":
-    #demo()
+    #import json
+    #s = Structer()    
+    #with open("structer.json", "w") as f:
+    #    f.write(json.dumps((s.application_spec())))
 
-    import json
-    s = Structer()    
-    with open("structer.json", "w") as f:
-        f.write(json.dumps((s.application_spec())))
+    demo()
