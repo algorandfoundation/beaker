6c8dd86a4ebbd5daee85d255a320ef31511770c1
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-27 13:50:46 -0400

    improved blob demo

--- examples/account_storage/main.py
+++ examples/account_storage/main.py
@@ -5,7 +5,6 @@ import algosdk.future.transaction as txns
 from algosdk.atomic_transaction_composer import *
 from pyteal import *
 from beaker import *
-from beaker.lib.storage import LocalBlob
 
 # Simple logic sig, will approve _any_ transaction
 # Used to expand our apps available state by
@@ -23,11 +22,8 @@ class KeySig(LogicSignature):
 # This allows us to use the local storage of the unique accounts
 # to get an extra 2k of storage for each account
 class DiskHungry(Application):
-    # Reserve 16 byte keys in local state
-    stuff = DynamicAccountStateValue(TealType.bytes, max_keys=16)
-
-    # Create a LocalBlob to be used for arbitrary read/writes of the 127*16 bytes avail
-    blob = LocalBlob()
+    # Reserve all 16 keys for the blob in local state
+    data = AccountStateBlob(max_keys=16)
 
     # Signal to beaker that this should be compiled
     # prior to compiling the main application
@@ -44,8 +40,33 @@ class DiskHungry(Application):
                 # and that its being rekeyed to us
                 Txn.rekey_to() == self.address,
             ),
-            # Zero out the local storage
-            self.blob.zero(Txn.sender()),
+            self.initialize_account_state(),
+        )
+
+    # Inline these
+    def byte_idx(self, bit_idx)->Int:
+        return bit_idx / Int(8)
+
+    def bit_in_byte_idx(self, bit_idx)->Int:
+        return bit_idx % Int(8)
+
+    @external
+    def flip_bit(self, nonce_acct: abi.Account, bit_idx: abi.Uint32):
+        return Seq(
+            # Read byte (take only the last byte of the 8 byte int it returns)
+            (byte := ScratchVar()).store(
+                Suffix(Itob(self.data[nonce_acct.address()].read_byte(self.byte_idx(bit_idx.get()))), Int(7))
+            ),
+            # Flip bit
+            byte.store(
+                SetBit(
+                    byte.load(),
+                    Int(7) - self.bit_in_byte_idx(bit_idx.get()),
+                    Not(GetBit(byte.load(), self.bit_in_byte_idx(bit_idx.get()))),
+                )
+            ),
+            # Write byte
+            self.data[nonce_acct.address()].write_byte(self.byte_idx(bit_idx.get()), Btoi(byte.load())),
         )
 
 
@@ -57,6 +78,7 @@ def demo():
     )
     # Create the app
     app_client.create()
+    app_client.fund(2*consts.algo)
 
     # Get the `precompile` wrapped LSig from the app instance
     tmpl_lsig: Precompile = cast(DiskHungry, app_client.app).tmpl_acct
@@ -64,17 +86,36 @@ def demo():
     # Create 10 random nonces for unique lsig accounts
     # and make them opt in to the app
     for _ in range(10):
-        create_account(app_client, tmpl_lsig, get_nonce())
+        nonce = get_nonce()
+        lsig_signer = tmpl_lsig.template_signer(nonce)
+        create_account_and_opt_in(app_client, lsig_signer, nonce)
+
+        # Max is 8 (bits per byte) * 127 (bytes per key) * 16 (max keys) == 16256 
+        idx =  16255 
+        app_client.call(DiskHungry.flip_bit, nonce_acct=lsig_signer.lsig.address(), bit_idx=idx)
+
+        acct_state = app_client.get_account_state(lsig_signer.lsig.address(), raw=True)
+        blob = b""
+        for x in range(16):
+            key = x.to_bytes(1, 'big')
+            blob +=  cast(bytes, acct_state[key])
+
+        assert int(blob[idx//8]) == 2**(idx%8)
+        print(f"bit set correctly at index {idx}")
+
+
+        
+
+
 
 
 def get_nonce(n: int = 10) -> bytes:
     return ("".join(random.choice(string.ascii_uppercase) for _ in range(n))).encode()
 
 
-def create_account(
-    app_client: client.ApplicationClient, lsig_precompile: Precompile, nonce: bytes
+def create_account_and_opt_in(
+    app_client: client.ApplicationClient, lsig_signer: LogicSigTransactionSigner, nonce: bytes
 ):
-    lsig_signer = lsig_precompile.template_signer(nonce)
     lsig_client = app_client.prepare(signer=lsig_signer)
 
     print(
@@ -113,10 +154,6 @@ def create_account(
     )
 
     atc.execute(app_client.client, 4)
-    print("Done, current local state:")
-    for k, v in lsig_client.get_account_state(raw=True).items():
-        print(f"\t{k}\t: {len(v)} bytes")
-    print()
 
 
 if __name__ == "__main__":
