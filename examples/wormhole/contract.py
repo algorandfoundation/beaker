c926e4e2c02c0f07d5463d19342be03b987fbb53
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-30 14:55:34 -0400

    adding initial wormhole transfer handler (#23)

---
+++ examples/wormhole/contract.py
@@ -0,0 +1,60 @@
+from typing import Final
+from pyteal import *
+from beaker import *
+
+if __name__ == "__main__":
+    from wormhole import ContractTransferVAA, WormholeTransfer
+else:
+    from .wormhole import ContractTransferVAA, WormholeTransfer
+
+
+class OracleDataCache(WormholeTransfer):
+    """
+    Stores price feed in application state keyed by timestamp
+
+    TODO: more than 64 vals lol
+    """
+
+    class OracleData(abi.NamedTuple):
+        timestamp: abi.Field[abi.Uint64]
+        price: abi.Field[abi.Uint64]
+        confidence: abi.Field[abi.Uint64]
+
+    prices: Final[DynamicApplicationStateValue] = DynamicApplicationStateValue(
+        stack_type=TealType.bytes, max_keys=64
+    )
+
+    def handle_transfer(
+        self, ctvaa: ContractTransferVAA, *, output: abi.DynamicBytes
+    ) -> Expr:
+        """
+        invoked from parent class `portal_transfer` after parsing the VAA into
+        abi vars
+        """
+        return Seq(
+            # TODO: assert foreign sender? Should be in provided contract?
+            # Do this once, since `get`` incurs a couple op penalty over `load`
+            (payload := ScratchVar()).store(ctvaa.payload.get()),
+            # Read vals from json
+            (timestamp := abi.Uint64()).set(
+                JsonRef.as_uint64(payload.load(), Bytes("ts"))
+            ),
+            (price := abi.Uint64()).set(
+                JsonRef.as_uint64(payload.load(), Bytes("price"))
+            ),
+            (confidence := abi.Uint64()).set(
+                JsonRef.as_uint64(payload.load(), Bytes("confidence"))
+            ),
+            # Construct named tuple for storage
+            (od := abi.make(OracleDataCache.OracleData)).set(
+                timestamp, price, confidence
+            ),
+            # Write to app state
+            self.prices[timestamp].set(od.encode()),
+            # echo the payload
+            output.set(ctvaa.payload),
+        )
+
+    @external
+    def lookup(self, ts: abi.Uint64, *, output: OracleData):
+        return output.decode(self.prices[ts].get_must())
---
+++ examples/wormhole/wormhole.py
@@ -0,0 +1,151 @@
+from abc import ABC, abstractmethod
+from typing import Literal
+
+from beaker import Application, external
+from pyteal import (
+    Reject,
+    abi,
+    Expr,
+    Seq,
+    ScratchVar,
+    Int,
+    Suffix,
+)
+
+
+def read_next(vaa: Expr, offset: int, t: abi.BaseType) -> tuple[int, Expr]:
+    size = t.type_spec().byte_length_static()
+    return offset + size, t.decode(vaa, start_index=Int(offset), length=Int(size))
+
+
+Bytes32 = abi.StaticBytes[Literal[32]]
+
+
+class ContractTransferVAA:
+    def __init__(self):
+        #: Version of VAA
+        self.version = abi.Uint8()
+        #: Which guardian set to be validated against
+        self.index = abi.Uint32()
+        #: How many signatures
+        self.siglen = abi.Uint8()
+        #: TS of message
+        self.timestamp = abi.Uint32()
+        #: Uniquifying
+        self.nonce = abi.Uint32()
+        #: The Id of the chain where the message originated
+        self.chain = abi.Uint16()
+        #: The address of the contract that emitted this message on the origin chain
+        self.emitter = abi.Address()
+        #: Unique integer representing the index, used for dedupe/ordering
+        self.sequence = abi.Uint64()
+
+        self.consistency = abi.Uint8()  # ?
+
+        #: Type of message
+        self.type = abi.Uint8()
+        #: amount of transfer
+        self.amount = abi.make(Bytes32)
+        #: asset transferred
+        self.contract = abi.make(Bytes32)
+        #: Id of the chain the token originated
+        self.from_chain = abi.Uint16()
+        #: Receiver of the token transfer
+        self.to_address = abi.Address()
+        #: Id of the chain where the token transfer should be redeemed
+        self.to_chain = abi.Uint16()
+        #: Amount to pay relayer
+        self.fee = abi.make(Bytes32)
+        #: Address that sent the transfer
+        self.from_address = abi.Address()
+
+        #: Arbitrary byte payload
+        self.payload = abi.DynamicBytes()
+
+    def decode(self, vaa: Expr) -> Expr:
+        offset = 0
+        ops: list[Expr] = []
+
+        offset, e = read_next(vaa, offset, self.version)
+        ops.append(e)
+
+        offset, e = read_next(vaa, offset, self.index)
+        ops.append(e)
+
+        offset, e = read_next(vaa, offset, self.siglen)
+        ops.append(e)
+
+        # Increase offset to skip over sigs && digest
+        # since these should be checked by the wormhole core contract
+        ops.append(
+            (digest_vaa := ScratchVar()).store(
+                Suffix(vaa, Int(offset) + (self.siglen.get() * Int(66)))
+            )
+        )
+
+        # Reset the offset now that we have const length elements
+        offset = 0
+        offset, e = read_next(digest_vaa.load(), offset, self.timestamp)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.nonce)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.chain)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.emitter)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.sequence)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.consistency)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.type)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.amount)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.contract)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.from_chain)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.to_address)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.to_chain)
+        ops.append(e)
+        offset, e = read_next(digest_vaa.load(), offset, self.from_address)
+        ops.append(e)
+        # Rest is payload
+        ops.append(self.payload.set(Suffix(digest_vaa.load(), Int(offset))))
+
+        return Seq(*ops)
+
+
+class WormholeTransfer(Application, ABC):
+    """Wormhole Payload3 Message handler
+
+    A Message transfer from another chain to Algorand  using the Wormhole protocol
+    will cause this contract to have it's `portal_transfer` method called.
+    """
+
+    @external
+    def portal_transfer(
+        self, vaa: abi.DynamicBytes, *, output: abi.DynamicBytes
+    ) -> Expr:
+        """
+        portal_transfer accepts a VAA containing information about the transfer and the payload.
+
+        To allow a more flexible interface we publicize that we output generic bytes
+        """
+        return Seq(
+            (ctvaa := ContractTransferVAA()).decode(vaa.get()),
+            self.handle_transfer(ctvaa, output=output),
+        )
+
+    @abstractmethod
+    def handle_transfer(
+        self, ctvaa: ContractTransferVAA, *, output: abi.DynamicBytes
+    ) -> Expr:
+        """
+
+        Should be overridden with whatever app specific stuff
+        needs to be done on transfer
+
+        """
+        return Reject()
