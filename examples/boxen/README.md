98fac40d5f74f8da62f4bfa8c120f6ddc658e7c8
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-09-03 11:28:11 -0400

    untested

---
+++ examples/boxen/README.md
@@ -0,0 +1,18 @@
+The DAO of Boxes
+----------------
+
+If a `map` is required a box per key should be used
+    - To know what the key to reference, ask the app to tell us what the key should be
+
+A common pattern will likely be a user map
+
+
+If a `list` is required it must be <128k to access the entire list
+    - For an 8 byte element, this may contain 16000 elements
+    - For a 32 byte element, this may contain 4000 elements
+    - For a 128 byte element, this may contain 1000 elements
+
+
+
+
+
--- examples/boxen/main.py
+++ examples/boxen/main.py
@@ -1,11 +1,102 @@
 from pyteal import *
 from beaker import *
 
+
+# Box Consensus params
+
+# BoxFlatMinBalance = 0.002500
+# BoxByteMinBalance = 0.000400
+
+# MaxBoxSize = 4 * 8096
+# MaxAppBoxReferences = 8
+# BytesPerBoxReference = 1024
+
+# Max box bytes accessible in 1 app call: 8k
+# Max box bytes accessible in 16 app calls: 128k
+
+# name = Bytes("name")
+# val = Bytes("val")
+# size = Int(1)
+# start = Int(1)
+#
+# BoxCreate(name, size) # Create a new box of size
+# BoxDelete(name) # Delete box
+#
+# BoxExtract(name, start, size) # Get `size` bytes from box starting from `start`
+# BoxReplace(name, start, val) # Overwrite whatever is in the box from start to len(val)
+# BoxPut(name, val) # Write all contents of `val` to box starting from 0
+#
+# BoxGet(name) # Get the full contents of this box (will panic >4k)
+# BoxLen(name) # Get the size of this box
+#
+
+# Use a box per member to denote membership parameters
+class MembershipRecord(abi.NamedTuple):
+    role: abi.Field[abi.Uint8]
+    voted: abi.Field[abi.Bool]
+
+
+class MapElement:
+    def __init__(self, key: Expr):
+        assert key.type_of() == TealType.bytes, TealTypeError(
+            key.type_of(), TealType.bytes
+        )
+        self.key = key
+
+    def get(self) -> Expr:
+        return Seq(maybe := BoxGet(self.key), Assert(maybe.hasValue()), maybe.value())
+
+    def set(self, val: abi.BaseType | Expr) -> Expr:
+        match val:
+            case abi.BaseType():
+                return BoxPut(self.key, val.encode())
+            case Expr():
+                if val.type_of() != TealType.bytes:
+                    raise TealTypeError(val.type_of(), TealType.bytes)
+                return BoxPut(self.key, val)
+
+    def delete(self) -> Expr:
+        return BoxDelete(self.key)
+
+
+class Mapping:
+    def __init__(self, key_type: type[abi.BaseType], value_type: type[abi.BaseType]):
+        self.key_type = key_type
+        self.value_type = value_type
+
+    def __getitem__(self, idx: abi.BaseType | Expr):
+        match idx:
+            case abi.BaseType():
+                return MapElement(idx.encode())
+            case Expr():
+                if idx.type_of() != TealType.bytes:
+                    raise TealTypeError(idx.type_of(), TealType.bytes)
+                return MapElement(idx)
+
+
 class Boxen(Application):
 
+    membership = Mapping(abi.Address, MembershipRecord)
+
     @external
-    def make_box(self, name: abi.String, size: abi.Uint32):
-        return Pop(BoxCreate(name.get(), size.get()))
+    def add_member(self):
+        return Seq(
+            (role := abi.Uint8()).set(Int(0)),
+            (voted := abi.Bool()).set(consts.FALSE),
+            (mr := MembershipRecord()).set(role, voted),
+            self.membership[Txn.sender()].set(mr),
+        )
+
+    @external(read_only=True)
+    def has_voted(self, member: abi.Address, *, output: abi.Bool):
+        return Seq(
+            (mr := MembershipRecord()).decode(self.membership[member].get()),
+            output.set(mr.voted),
+        )
+
+    @external
+    def remove_member(self):
+        return Pop(self.membership[Txn.sender()].delete())
 
 
 if __name__ == "__main__":
