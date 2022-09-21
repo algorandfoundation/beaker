95ab56714dd0453fe86a56b9e80289eabaf4c5f0
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-09-21 15:34:51 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="beaker",
-    version="0.1.12-alpha",
+    version="0.1.13-alpha",
     python_requires=">=3.10",
     packages=find_packages(),
     license="MIT",
