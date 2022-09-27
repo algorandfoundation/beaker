5b80ddb1fdd42f3e8dd2919a087b42507c2071c7
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-09-27 13:14:22 -0500

    remove unused import

--- beaker/lib/storage/__init__.py
+++ beaker/lib/storage/__init__.py
@@ -2,4 +2,3 @@ from .local_blob import LocalBlob
 from .global_blob import GlobalBlob
 from .mapping import Mapping, MapElement
 from .listing import Listing, ListElement
-from .util import resize_box
