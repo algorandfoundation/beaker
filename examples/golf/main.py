ad30ddbd80fe5d28ba600d0d4a99d42271397f01
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-11-02 07:55:13 -0400

    cleanup

--- examples/golf/main.py
+++ examples/golf/main.py
@@ -1,16 +1,10 @@
 import base64
+import random
 from typing import Final
 from algosdk import *
 from pyteal import *
 from beaker import *
 from beaker.lib.math import max
-
-
-@Subroutine(TealType.uint64)
-def div_round(a, b) -> Expr:
-    quo = a / b
-    mod = a % b
-    return If(mod > (b / Int(2)), quo + Int(1), quo)
 
 
 class NumberOrder(Application):
@@ -71,30 +65,24 @@ class NumberOrder(Application):
     @internal(TealType.uint64)
     def binary_search(self, val: Expr, arr: Expr, start: Expr, end: Expr) -> Expr:
         return Seq(
-            Comment("in bin"),
             If(start > end, Return(start)),
-            Comment("start<=end"),
             If(
                 start == end,
                 Return(
                     start + If(self.lookup_element(arr, start) > val, Int(0), Int(1))
                 ),
             ),
-            Comment("start!=end"),
             (mid := ScratchVar()).store((start + end) / Int(2)),
             (midval := ScratchVar()).store(self.lookup_element(arr, mid.load())),
-            Comment("gotvals"),
             If(midval.load() < val)
             .Then(
-                Comment("midval<val"),
                 self.binary_search(val, arr, mid.load() + Int(1), end),
             )
             .ElseIf(midval.load() > val)
             .Then(
-                Comment("midval>val"),
                 self.binary_search(val, arr, start, max(Int(1), mid.load()) - Int(1)),
             )
-            .Else(Comment("midval=val"), mid.load()),
+            .Else(mid.load()),
         )
 
     def lookup_element(self, buff: Expr, idx: Expr):
@@ -116,50 +104,19 @@ class NumberOrder(Application):
         )
 
 
-def binary_search(arr, val, start, end):
-    if start > end:
-        return start
-
-    if start == end:
-        if arr[start] > val:
-            return start
-        return start + 1
-
-    mid = (start + end) // 2
-
-    if arr[mid] < val:
-        return binary_search(arr, val, mid + 1, end)
-    elif arr[mid] > val:
-        return binary_search(arr, val, start, mid - 1)
-    else:
-        return mid
-
-
 if __name__ == "__main__":
-    # arr = []
-    # for x in [37, 23, 0, 31, 22, 17, 12, 72, 31, 46, 100, 88, 54]:
-    #    j = binary_search(arr, x, 0, len(arr) - 1)
-    #    arr = arr[:j] + [x] + arr[j:]
-    # print(arr)
 
-    accts = sandbox.get_accounts()
-    acct = accts.pop()
-    acct2 = accts.pop()
+    acct = sandbox.get_accounts().pop()
 
     app_client = client.ApplicationClient(
         sandbox.get_algod_client(), NumberOrder(version=8), signer=acct.signer
     )
-    NumberOrder().dump("./artifacts")
-    app_client.create()
-    app_client.fund(100 * consts.algo)
-    print(f"AppID: {app_client.app_id}  AppAddr: {app_client.app_addr}")
+    app_client.app.dump("./artifacts")
 
+    # Create 4 box refs since we need to touch 4k
     boxes = [[app_client.app_id, "BoxA"]] * 4
-    result = app_client.call(
-        NumberOrder.box_create_test,
-        boxes=boxes,
-    )
 
+    # Util funcs
     def decode_int(b: str) -> int:
         return int.from_bytes(base64.b64decode(b), "big")
 
@@ -172,8 +129,6 @@ if __name__ == "__main__":
             val=num,
             boxes=boxes,
         )
-        print([decode_int(v) for v in result.tx_info["logs"][:-1]])
-        print(result.return_value)
         return decode_budget(result.tx_info)
 
     def get_box() -> list[int]:
@@ -182,27 +137,40 @@ if __name__ == "__main__":
         )
 
         vals = []
-        print(box_contents["value"])
         data = base64.b64decode(box_contents["value"])
         for idx in range(len(data) // 8):
             vals.append(int.from_bytes(data[idx * 8 : (idx + 1) * 8], "big"))
 
         return vals
 
-    import random
+    ##########
 
+    # Create && fund app acct
+    app_client.create()
+    app_client.fund(100 * consts.algo)
+    print(f"AppID: {app_client.app_id}  AppAddr: {app_client.app_addr}")
+
+    # Make App Create box
+    result = app_client.call(
+        NumberOrder.box_create_test,
+        boxes=boxes,
+    )
+
+    # Shuffle 0-511
     nums = list(range(512))
     random.shuffle(nums)
     budgets = []
     for idx, n in enumerate(nums):
-        print(f"Iteration {idx}: {n}")
+        if idx % 32 == 0:
+            print(f"Iteration {idx}: {n}")
         budgets.append(add_number(n))
 
     print(budgets)
-    box = get_box()
-    print(box)
+
+    # Get contents of box
+    print(box := get_box())
+    # Make sure its sorted
     mx = 0
     for x in box:
-        print(x)
         assert mx <= x
         mx = x
