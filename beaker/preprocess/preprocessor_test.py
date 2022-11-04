3728a43c5b1a03cefbe41ae0abed24b14cb58a4f
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-11-04 15:53:17 -0400

    working with output

--- beaker/preprocess/preprocessor_test.py
+++ beaker/preprocess/preprocessor_test.py
@@ -6,72 +6,63 @@ from .preprocessor import Preprocessor
 from .builtins import *
 
 
-# def test_parse_method():
-#    def meth():
-#        x = 3
-#        y = 2**2
-#
-#        # Augmented assignment (load,op,store)
-#        x += 3
-#        x *= 3
-#
-#        # Both mapped to truncated division
-#        x /= 3
-#        x //= 3
-#
-#        z = "ok"
-#
-#        while y > 0:
-#            y -= 1
-#
-#        # `range` is a "builtin" we provide
-#        for _ in range(3):
-#            # maps to concat if the type is a string
-#            z += "no way"
-#
-#        if x * y:
-#            return 1
-#
-#        return x
-#
-#    pp = Preprocessor(meth)
-#    print(pp.body)
-#    print(compileTeal(pp.body, mode=Mode.Application, version=8))
-#
-#
-# def test_built_ins():
-#    # TODO: all the others
-#    def meth():
-#        app_put("ok", 123)
-#        x = app_get("ok")
-#        app_del("ok")
-#        return x
-#
-#    pp = Preprocessor(meth)
-#    print(pp.body)
-#    print(compileTeal(pp.body, mode=Mode.Application, version=8))
-#
-#
+def test_parse_method():
+   def meth():
+       x = 3
+       y = 2**2
+
+       # Augmented assignment (load,op,store)
+       x += 3
+       x *= 3
+
+       # Both mapped to truncated division
+       x /= 3
+       x //= 3
+
+       z = "ok"
+
+       while y > 0:
+           y -= 1
+
+       # `range` is a "builtin" we provide
+       for _ in range(3):
+           # maps to concat if the type is a string
+           z += "no way"
+
+       if x * y:
+           return 1
+
+       return x
+
+   pp = Preprocessor(meth)
+   print(pp.body)
+   print(compileTeal(pp.body, mode=Mode.Application, version=8))
+
+
+def test_built_ins():
+   # TODO: all the others
+   def meth():
+       app_put("ok", 123)
+       x = app_get("ok")
+       app_del("ok")
+       return x
+
+   pp = Preprocessor(meth)
+   print(pp.body)
+   print(compileTeal(pp.body, mode=Mode.Application, version=8))
+
+
 def test_app():
-    def meth() -> int:
-        x = 2
-        return x
-
-    pp = Preprocessor(meth)
-    print(pp.body)
-
     class App(Application):
-        # @external(translate=True)
-        # def no_args_no_output(self):
-        #    x = 2
-        #    x += 3
+        @external(translate=True)
+        def no_args_no_output(self):
+           x = 2
+           x += 3
 
-        # @external
-        # def no_args_yes_output_pt(self, *, output: abi.Uint64) -> Expr:
-        #    return output.set(Int(2))
+        @external
+        def no_args_yes_output_pt(self, *, output: abi.Uint64) -> Expr:
+           return output.set(Int(2))
 
-        # {'ops': [TealOp(err)], 'incoming': [TealConditionalBlock([TealOp(txna, 'ApplicationArgs', 0), TealOp(method, '"no_args_yes_output_pt()uint64"'), TealOp(==)], true=TealSimpleBlock([TealOp(txn, 'OnCompletion'), TealOp(int, 'NoOp'), TealOp(==), TealOp(txn, 'ApplicationID'), TealOp(int, 0), TealOp(!=), TealOp(&&), TealOp(assert), TealOp(callsub, <pyteal.SubroutineDefinition object at 0x7f99a74b3610>), TealOp(store, ScratchSlot(258)), TealOp(byte, '0x151f7c75'), TealOp(load, ScratchSlot(258)), TealOp(itob), TealOp(concat), TealOp(log), TealOp(int, 1), TealOp(return)], next=TealSimpleBlock([], next=None)), false=TealSimpleBlock([TealOp(err)], next=None))], 'nextBlock': None, 'visited': False}
-        # {'ops': [TealOp(int, 2), TealOp(retsub)], 'incoming': [], 'nextBlock': None, 'visited': False}
 
         @external(translate=True)
         def no_args_yes_output_py(self) -> int:
