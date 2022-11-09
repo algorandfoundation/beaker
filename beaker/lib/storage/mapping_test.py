a122e1836fd1de52394cde7d1c23d80a8b49355f
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-11-09 08:54:57 -0500

    check expr

--- beaker/lib/storage/mapping_test.py
+++ beaker/lib/storage/mapping_test.py
@@ -1,10 +1,33 @@
 import pyteal as pt
-from .mapping import Mapping
+from .mapping import Mapping, MapElement
 from beaker.application import Application
 from beaker.decorators import external
 
 
+options = pt.CompileOptions(version=pt.MAX_TEAL_VERSION, mode=pt.Mode.Application)
+
+
 def test_mapping():
+    m = Mapping(pt.abi.Address, pt.abi.Uint64)
+    assert m._key_type == pt.abi.Address
+    assert m._key_type_spec == pt.abi.AddressTypeSpec()
+
+    assert m._value_type == pt.abi.Uint64
+    assert m._value_type_spec == pt.abi.Uint64TypeSpec()
+
+    item = m[pt.Txn.sender()]
+    assert isinstance(item, MapElement) 
+
+    expected, _ = pt.Seq(bx := pt.BoxGet(pt.Txn.sender()), pt.Assert(bx.hasValue()), bx.value()).__teal__(options)
+    actual, _ = item.get().__teal__(options)
+
+    with pt.TealComponent.Context.ignoreExprEquality(), pt.TealComponent.Context.ignoreScratchSlotEquality():
+        assert actual == expected
+
+
+
+def test_app_mapping():
+
     class T(Application):
         m = Mapping(pt.abi.Address, pt.abi.Uint64)
 
@@ -14,3 +37,5 @@ def test_mapping():
 
     t = T()
     assert len(t.approval_program) > 0
+
+
