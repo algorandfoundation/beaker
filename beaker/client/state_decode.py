adb26182e6d225a18dd9d8fc2263f48f785c69c9
Joe Polny <joepolny@gmail.com>
2022-11-17 18:16:12 -0500

    handle "action" and "type" keys in decode_state

--- beaker/client/state_decode.py
+++ beaker/client/state_decode.py
@@ -25,12 +25,18 @@ def decode_state(
         key: str | bytes = raw_key if raw else str_or_hex(raw_key)
         val: str | bytes | int
 
-        match sv["value"]["type"]:
+        action = (
+            sv["value"]["action"] if "action" in sv["value"] else sv["value"]["type"]
+        )
+
+        match action:
             case 1:
                 raw_val = b64decode(sv["value"]["bytes"])
                 val = raw_val if raw else str_or_hex(raw_val)
             case 2:
                 val = sv["value"]["uint"]
+            case 3:
+                val = None
 
         decoded_state[key] = val
     return decoded_state
