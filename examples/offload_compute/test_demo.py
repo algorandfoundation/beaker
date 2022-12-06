880acc4ac58e1d101f8a55ba61bf4ac4de9d7030
Adam Chidlow <achidlow@users.noreply.github.com>
2022-12-06 12:37:46 +0800

    beaker productionisation, the first
    - output stability tests: basic implementation for now, and re-use examples for this purpose.
      in the future, we will need to modify this approach, but this will allow us to make changes with some confidence in the meantime
    - use poetry for dependency / virtual env management + CI/CD tasks. this will remove examples + tests from package.
    - enable code coverage, moving tests to a separate folder to achieve easier measurement
    - use pre-commit for easier dev experience
    - update dev tool versions to the latest possible without large changes
    - speed up CI/CD time from ~10m to ~2m by using pre-built docker images
    - remove Makefile approach and a ./scripts/gen.sh to make development of Beaker on Windows a little easier.
      the former has been replaced by pre-commit, poetry run, etc, the latter by pytest

--- examples/offload_compute/test_demo.py
+++ examples/offload_compute/test_demo.py
@@ -1,5 +1,11 @@
-from .main import demo
+from examples.offload_compute.main import demo, EthChecker
+from tests.conftest import check_application_artifacts_output_stability
 
 
 def test_demo():
     demo()
+
+
+def test_output_stability():
+    app = EthChecker()
+    check_application_artifacts_output_stability(app)
--- examples/wormhole/test_demo.py
+++ examples/wormhole/test_demo.py
@@ -1,5 +1,12 @@
-from .main import demo
+from tests.conftest import check_application_artifacts_output_stability
+from examples.wormhole.contract import OracleDataCache
+from examples.wormhole.main import demo
 
 
 def test_demo():
     demo()
+
+
+def test_output_stability():
+    app = OracleDataCache()
+    check_application_artifacts_output_stability(app, dir_name="spec")
