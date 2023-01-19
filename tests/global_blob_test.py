87f4616f2ade60f0d9193c8c2e076bd5b76bd2b5
Adam Chidlow <achidlow@users.noreply.github.com>
2023-01-19 15:12:52 +0800

    typing fixes + unit test updates

--- tests/global_blob_test.py
+++ tests/global_blob_test.py
@@ -1,7 +1,6 @@
 import pytest
 import pyteal as pt
 import beaker as bkr
-from beaker.application import State
 
 from beaker.testing.unit_testing_helpers import UnitTestingApp, assert_output
 
@@ -9,37 +8,41 @@ from beaker.lib.storage.global_blob import GlobalBlob
 from beaker.lib.storage.blob import blob_page_size
 
 
-class GlobalBlobState(State):
+class GlobalBlobTest(UnitTestingApp):
     lb = bkr.ReservedApplicationStateValue(pt.TealType.bytes, max_keys=64)
     blob = GlobalBlob()
 
 
 def test_global_blob_zero():
-    app = UnitTestingApp(state=GlobalBlobState())
+    class LBZero(GlobalBlobTest):
+        def __init__(self):
+            super().__init__()
 
-    @app.external()
-    def unit_test(*, output: pt.abi.DynamicArray[pt.abi.Byte]):
-        return pt.Seq(
-            app.state.blob.zero(),
-            (s := pt.abi.String()).set(app.state.blob.read(pt.Int(0), pt.Int(64))),
-            output.decode(s.encode()),
-        )
+            @self.external
+            def unit_test(*, output: pt.abi.DynamicArray[pt.abi.Byte]):
+                return pt.Seq(
+                    self.blob.zero(),
+                    (s := pt.abi.String()).set(self.blob.read(pt.Int(0), pt.Int(64))),
+                    output.decode(s.encode()),
+                )
 
     expected = list(bytes(64))
-    assert_output(app, [], [expected], opups=1)
+    assert_output(LBZero(), [], [expected], opups=1)
 
 
 def test_global_blob_write_read():
     class LB(GlobalBlobTest):
-        @bkr.external
-        def unit_test(self, *, output: pt.abi.DynamicArray[pt.abi.Byte]):
-            return pt.Seq(
-                self.blob.zero(),
-                self.blob.write(pt.Int(0), pt.Bytes("deadbeef" * 8)),
-                self.blob.write(pt.Int(0), pt.Bytes("deadbeef" * 8)),
-                (s := pt.abi.String()).set(self.blob.read(pt.Int(32), pt.Int(40))),
-                output.decode(s.encode()),
-            )
+        def __init__(self):
+            super().__init__()
+
+            @self.external
+            def unit_test(*, output: pt.abi.DynamicArray[pt.abi.Byte]):
+                return pt.Seq(
+                    self.blob.zero(),
+                    self.blob.write(pt.Int(0), pt.Bytes("deadbeef" * 8)),
+                    (s := pt.abi.String()).set(self.blob.read(pt.Int(32), pt.Int(40))),
+                    output.decode(s.encode()),
+                )
 
     expected = list(b"deadbeef")
     assert_output(LB(), [], [expected], opups=1)
@@ -47,14 +50,17 @@ def test_global_blob_write_read():
 
 def test_global_blob_write_read_boundary():
     class LB(GlobalBlobTest):
-        @bkr.external
-        def unit_test(self, *, output: pt.abi.DynamicArray[pt.abi.Byte]):
-            return pt.Seq(
-                self.blob.zero(),
-                self.blob.write(pt.Int(0), pt.BytesZero(pt.Int(381))),
-                (s := pt.abi.String()).set(self.blob.read(pt.Int(32), pt.Int(40))),
-                output.decode(s.encode()),
-            )
+        def __init__(self):
+            super().__init__()
+
+            @self.external
+            def unit_test(*, output: pt.abi.DynamicArray[pt.abi.Byte]):
+                return pt.Seq(
+                    self.blob.zero(),
+                    self.blob.write(pt.Int(0), pt.BytesZero(pt.Int(381))),
+                    (s := pt.abi.String()).set(self.blob.read(pt.Int(32), pt.Int(40))),
+                    output.decode(s.encode()),
+                )
 
     expected = list(bytes(8))
     assert_output(LB(), [], [expected], opups=1)
@@ -62,16 +68,19 @@ def test_global_blob_write_read_boundary():
 
 def test_global_blob_write_read_past_end():
     class LB(GlobalBlobTest):
-        @bkr.external
-        def unit_test(self, *, output: pt.abi.DynamicArray[pt.abi.Byte]):
-            return pt.Seq(
-                self.blob.zero(),
-                self.blob.write(pt.Int(0), pt.Bytes("deadbeef" * 8)),
-                (s := pt.abi.String()).set(
-                    self.blob.read(pt.Int(0), pt.Int(blob_page_size * 64))
-                ),
-                output.decode(s.encode()),
-            )
+        def __init__(self):
+            super().__init__()
+
+            @self.external
+            def unit_test(*, output: pt.abi.DynamicArray[pt.abi.Byte]):
+                return pt.Seq(
+                    self.blob.zero(),
+                    self.blob.write(pt.Int(0), pt.Bytes("deadbeef" * 8)),
+                    (s := pt.abi.String()).set(
+                        self.blob.read(pt.Int(0), pt.Int(blob_page_size * 64))
+                    ),
+                    output.decode(s.encode()),
+                )
 
     expected = list(bytes(8))
 
@@ -83,13 +92,16 @@ def test_global_blob_set_get():
     num = 123
 
     class LB(GlobalBlobTest):
-        @bkr.external
-        def unit_test(self, *, output: pt.abi.Uint8):
-            return pt.Seq(
-                self.blob.zero(),
-                self.blob.set_byte(pt.Int(32), pt.Int(num)),
-                output.set(self.blob.get_byte(pt.Int(32))),
-            )
+        def __init__(self):
+            super().__init__()
+
+            @self.external
+            def unit_test(*, output: pt.abi.Uint8):
+                return pt.Seq(
+                    self.blob.zero(),
+                    self.blob.set_byte(pt.Int(32), pt.Int(num)),
+                    output.set(self.blob.get_byte(pt.Int(32))),
+                )
 
     expected = [num]
     assert_output(LB(), [], expected)
@@ -99,13 +111,16 @@ def test_global_blob_set_past_end():
     num = 123
 
     class LB(GlobalBlobTest):
-        @bkr.external
-        def unit_test(self, *, output: pt.abi.Uint8):
-            return pt.Seq(
-                self.blob.zero(),
-                self.blob.set_byte(pt.Int(blob_page_size * 64), pt.Int(num)),
-                output.set(self.blob.get_byte(pt.Int(32))),
-            )
+        def __init__(self):
+            super().__init__()
+
+            @self.external
+            def unit_test(*, output: pt.abi.Uint8):
+                return pt.Seq(
+                    self.blob.zero(),
+                    self.blob.set_byte(pt.Int(blob_page_size * 64), pt.Int(num)),
+                    output.set(self.blob.get_byte(pt.Int(32))),
+                )
 
     expected = [num]
 
