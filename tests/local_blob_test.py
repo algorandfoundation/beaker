4ca8a1f83957c372000417b124d6ebd8417f8b38
Adam Chidlow <achidlow@users.noreply.github.com>
2023-01-19 15:07:01 +0800

    typing fixes + unit test updates

--- tests/local_blob_test.py
+++ tests/local_blob_test.py
@@ -14,97 +14,101 @@ class LocalBlobTest(UnitTestingApp):
 
 
 def test_local_blob_zero():
-    class LBZero(LocalBlobTest):
-        @bkr.external
-        def unit_test(self, *, output: pt.abi.DynamicArray[pt.abi.Byte]):
-            return pt.Seq(
-                self.blob.zero(),
-                (s := pt.abi.String()).set(self.blob.read(pt.Int(0), pt.Int(64))),
-                output.decode(s.encode()),
-            )
+    app = LocalBlobTest()
+
+    @app.external
+    def unit_test(*, output: pt.abi.DynamicArray[pt.abi.Byte]):
+        return pt.Seq(
+            app.blob.zero(),
+            (s := pt.abi.String()).set(app.blob.read(pt.Int(0), pt.Int(64))),
+            output.decode(s.encode()),
+        )
 
     expected = list(bytes(64))
-    assert_output(LBZero(), [], [expected])
+    assert_output(app, [], [expected])
 
 
 def test_local_blob_write_read():
-    class LB(LocalBlobTest):
-        @bkr.external
-        def unit_test(self, *, output: pt.abi.DynamicArray[pt.abi.Byte]):
-            return pt.Seq(
-                self.blob.zero(),
-                self.blob.write(pt.Int(0), pt.Bytes("deadbeef" * 8)),
-                (s := pt.abi.String()).set(self.blob.read(pt.Int(32), pt.Int(40))),
-                output.decode(s.encode()),
-            )
+    app = LocalBlobTest()
+
+    @app.external
+    def unit_test(*, output: pt.abi.DynamicArray[pt.abi.Byte]):
+        return pt.Seq(
+            app.blob.zero(),
+            app.blob.write(pt.Int(0), pt.Bytes("deadbeef" * 8)),
+            (s := pt.abi.String()).set(app.blob.read(pt.Int(32), pt.Int(40))),
+            output.decode(s.encode()),
+        )
 
     expected = list(b"deadbeef")
-    assert_output(LB(), [], [expected])
+    assert_output(app, [], [expected])
 
 
 def test_local_blob_write_read_boundary():
-    class LB(LocalBlobTest):
-        @bkr.external
-        def unit_test(self, *, output: pt.abi.DynamicArray[pt.abi.Byte]):
-            return pt.Seq(
-                self.blob.zero(pt.Int(0)),
-                self.blob.write(pt.Int(0), pt.BytesZero(pt.Int(blob_page_size * 3))),
-                (s := pt.abi.String()).set(self.blob.read(pt.Int(32), pt.Int(40))),
-                output.decode(s.encode()),
-            )
+    app = LocalBlobTest()
+
+    @app.external
+    def unit_test(*, output: pt.abi.DynamicArray[pt.abi.Byte]):
+        return pt.Seq(
+            app.blob.zero(pt.Int(0)),
+            app.blob.write(pt.Int(0), pt.BytesZero(pt.Int(blob_page_size * 3))),
+            (s := pt.abi.String()).set(app.blob.read(pt.Int(32), pt.Int(40))),
+            output.decode(s.encode()),
+        )
 
     expected = list(bytes(8))
-    assert_output(LB(), [], [expected])
+    assert_output(app, [], [expected])
 
 
 def test_local_blob_write_read_past_end():
-    class LB(LocalBlobTest):
-        @bkr.external
-        def unit_test(self, *, output: pt.abi.DynamicArray[pt.abi.Byte]):
-            return pt.Seq(
-                self.blob.zero(),
-                self.blob.write(pt.Int(0), pt.Bytes("deadbeef" * 8)),
-                (s := pt.abi.String()).set(
-                    self.blob.read(pt.Int(0), self.blob.max_bytes)
-                ),
-                output.decode(s.encode()),
-            )
+    app = LocalBlobTest()
+
+    @app.external
+    def unit_test(*, output: pt.abi.DynamicArray[pt.abi.Byte]):
+        return pt.Seq(
+            app.blob.zero(),
+            app.blob.write(pt.Int(0), pt.Bytes("deadbeef" * 8)),
+            (s := pt.abi.String()).set(app.blob.read(pt.Int(0), app.blob.max_bytes)),
+            output.decode(s.encode()),
+        )
 
     expected = list(bytes(8))
 
     with pytest.raises(bkr.client.LogicException):
-        assert_output(LB(), [], [expected])
+        assert_output(app, [], [expected])
 
 
 def test_local_blob_set_get():
     num = 123
 
-    class LB(LocalBlobTest):
-        @bkr.external
-        def unit_test(self, *, output: pt.abi.Uint8):
-            return pt.Seq(
-                self.blob.zero(),
-                self.blob.set_byte(pt.Int(32), pt.Int(num)),
-                output.set(self.blob.get_byte(pt.Int(32))),
-            )
+    app = LocalBlobTest()
+
+    @app.external
+    def unit_test(*, output: pt.abi.Uint8):
+        return pt.Seq(
+            app.blob.zero(),
+            app.blob.set_byte(pt.Int(32), pt.Int(num)),
+            output.set(app.blob.get_byte(pt.Int(32))),
+        )
 
     expected = [num]
-    assert_output(LB(), [], expected)
+    assert_output(app, [], expected)
 
 
 def test_local_blob_set_past_end():
     num = 123
 
-    class LB(LocalBlobTest):
-        @bkr.external
-        def unit_test(self, *, output: pt.abi.Uint8):
-            return pt.Seq(
-                self.blob.zero(),
-                self.blob.set_byte(self.blob.max_bytes, pt.Int(num)),
-                output.set(self.blob.get_byte(pt.Int(32))),
-            )
+    app = LocalBlobTest()
+
+    @app.external
+    def unit_test(*, output: pt.abi.Uint8):
+        return pt.Seq(
+            app.blob.zero(),
+            app.blob.set_byte(app.blob.max_bytes, pt.Int(num)),
+            output.set(app.blob.get_byte(pt.Int(32))),
+        )
 
     expected = [num]
 
     with pytest.raises(bkr.client.LogicException):
-        assert_output(LB(), [], expected)
+        assert_output(app, [], expected)
