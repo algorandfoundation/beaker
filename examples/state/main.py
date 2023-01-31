036bd80f074527a23e7075fd1612c1f98d9fe8c8
Adam Chidlow <achidlow@users.noreply.github.com>
2023-01-31 14:17:31 +0800

    hmmm

--- examples/state/main.py
+++ examples/state/main.py
@@ -28,38 +28,38 @@ def demo():
     app_client.opt_in()
     print("Opted in")
 
-    app_client.call(StateExample.set_account_state_val, v=123)
-    app_client.call(StateExample.incr_account_state_val, v=1)
-    result = app_client.call(StateExample.get_account_state_val)
+    app_client.call("set_account_state_val", v=123)
+    app_client.call("incr_account_state_val", v=1)
+    result = app_client.call("get_account_state_val")
     print(f"Set/get acct state result: {result.return_value}")
 
-    app_client.call(StateExample.set_reserved_account_state_val, k=123, v="stuff")
-    result = app_client.call(StateExample.get_reserved_account_state_val, k=123)
+    app_client.call("set_reserved_account_state_val", k=123, v="stuff")
+    result = app_client.call("get_reserved_account_state_val", k=123)
     print(f"Set/get dynamic acct state result: {result.return_value}")
 
     try:
-        app_client.call(StateExample.set_app_state_val, v="Expect fail")
+        app_client.call("set_app_state_val", v="Expect fail")
     except LogicException as e:
         print(f"Task failed successfully: {e}")
-        result = app_client.call(StateExample.get_app_state_val)
+        result = app_client.call("get_app_state_val")
         print(f"Set/get app state result: {result.return_value}")
 
-    app_client.call(StateExample.set_reserved_app_state_val, k=15, v=123)
-    result = app_client.call(StateExample.get_reserved_app_state_val, k=15)
+    app_client.call("set_reserved_app_state_val", k=15, v=123)
+    result = app_client.call("get_reserved_app_state_val", k=15)
     print(f"Set/get dynamic app state result: {result.return_value}")
 
     msg = "write this message please and make it readable"
 
     # Account state blob
-    app_client.call(StateExample.write_acct_blob, v=msg)
-    result = app_client.call(StateExample.read_acct_blob)
+    app_client.call("write_acct_blob", v=msg)
+    result = app_client.call("read_acct_blob")
     got_msg = bytes(result.return_value[: len(msg)]).decode()
     assert msg == got_msg
     print(f"wrote and read the message to account state {got_msg}")
 
     # App state blob
-    app_client.call(StateExample.write_app_blob, v=msg)
-    # result = app_client.call(StateExample.read_app_blob)
+    app_client.call("write_app_blob", v=msg)
+    # result = app_client.call("read_app_blob")
     # got_msg = bytes(result.return_value[: len(msg)]).decode()
     # assert msg == got_msg
     print(f"wrote and read the message to application state {got_msg}")
