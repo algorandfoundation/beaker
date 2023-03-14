8f472fde9a37cacf88bd09f78642ee35334b913e
Adam Chidlow <achidlow@users.noreply.github.com>
2023-03-14 16:18:55 +0800

    tests: simplify imports

--- examples/simple/test_demo.py
+++ examples/simple/test_demo.py
@@ -4,29 +4,33 @@ from beaker import Application
 
 from tests.conftest import check_application_artifacts_output_stability
 
-from examples.simple.calculator import calculator_app
-from examples.simple.calculator import demo as calc_demo
-from examples.simple.counter import counter_app
-from examples.simple.counter import demo as count_demo
-from examples.simple.decorators import external_example_app
-from examples.simple.hello import demo as hello_demo
-from examples.simple.hello import hello_app
+from examples.simple import calculator, counter, decorators, hello
 
 
 def test_calc() -> None:
-    calc_demo()
+    calculator.demo()
 
 
 def test_count() -> None:
-    count_demo()
+    counter.demo()
 
 
 def test_hello() -> None:
-    hello_demo()
+    hello.demo()
+
+
+def test_decorators() -> None:
+    decorators.demo()
 
 
 @pytest.mark.parametrize(
-    "app", [calculator_app, counter_app, hello_app, external_example_app]
+    "app",
+    [
+        calculator.calculator_app,
+        counter.counter_app,
+        hello.hello_app,
+        decorators.external_example_app,
+    ],
 )
 def test_output_stability(app: Application) -> None:
     check_application_artifacts_output_stability(app, dir_per_test_file=False)
