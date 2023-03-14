bb8fcdf0d86a313e7d1f3257d1f95b64367eaee0
Adam Chidlow <achidlow@users.noreply.github.com>
2023-03-14 15:32:07 +0800

    docs: refactor "opup" example to be exemplary for v1
    - use recommended import structure for beaker, pyteal, and app(s)
    - use demo.py:main() vs main.py:demo()

--- examples/opup/test_demo.py
+++ examples/opup/test_demo.py
@@ -1,12 +1,11 @@
 from tests.conftest import check_application_artifacts_output_stability
 
-from examples.opup.contract import expensive_app
-from examples.opup.main import demo
+from examples.opup import contract, demo
 
 
 def test_demo() -> None:
-    demo()
+    demo.main()
 
 
 def test_output_stability() -> None:
-    check_application_artifacts_output_stability(expensive_app, dir_per_test_file=False)
+    check_application_artifacts_output_stability(contract.app, dir_per_test_file=False)
