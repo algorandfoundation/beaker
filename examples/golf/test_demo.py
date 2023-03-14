18ed831eced3cd639d93edd5b61f61522572d865
Adam Chidlow <achidlow@users.noreply.github.com>
2023-03-14 14:42:19 +0800

    docs: refactor "golf" example to be exemplary for v1
    - move demo code to it's own file

--- examples/golf/test_demo.py
+++ examples/golf/test_demo.py
@@ -1,11 +1,13 @@
 from tests.conftest import check_application_artifacts_output_stability
 
-from examples.golf.sorted_integers import app, demo
+from examples.golf import demo, sorted_integers
 
 
 def test_demo() -> None:
-    demo()
+    demo.main()
 
 
 def test_output_stability() -> None:
-    check_application_artifacts_output_stability(app, dir_per_test_file=False)
+    check_application_artifacts_output_stability(
+        sorted_integers.app, dir_per_test_file=False
+    )
