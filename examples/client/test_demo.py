7aa14a4ab3198e989b70757d46032c078b0e2a22
Adam Chidlow <achidlow@users.noreply.github.com>
2023-03-14 13:38:24 +0800

    docs: refactor "client" example to be exemplary for v1
    - move contract into its own file
    - use recommended import structure for beaker, pyteal, and app(s)
    - use demo.py:main() vs main.py:demo()
    - rename app to be less generic

--- examples/client/test_demo.py
+++ examples/client/test_demo.py
@@ -1,11 +1,11 @@
 from tests.conftest import check_application_artifacts_output_stability
 
-from examples.client.main import demo, my_app
+from examples.client import demo, nicknames
 
 
 def test_demo() -> None:
-    demo()
+    demo.main()
 
 
 def test_output_stability() -> None:
-    check_application_artifacts_output_stability(my_app, dir_per_test_file=False)
+    check_application_artifacts_output_stability(nicknames.app, dir_per_test_file=False)
