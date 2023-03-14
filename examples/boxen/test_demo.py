d406680a59fe0daf7925c8d70617ecb2ee683ecb
Adam Chidlow <achidlow@users.noreply.github.com>
2023-03-14 13:26:36 +0800

    docs: refactor "boxen" example
    - use demo.py:main() vs main.py:demo()

--- examples/boxen/test_demo.py
+++ examples/boxen/test_demo.py
@@ -1,10 +1,10 @@
 from tests.conftest import check_application_artifacts_output_stability
 
-from examples.boxen import app_member, main, membership_club
+from examples.boxen import app_member, demo, membership_club
 
 
 def test_demo() -> None:
-    main.demo()
+    demo.main()
 
 
 def test_membership_club_output_stability() -> None:
