fd1146077689b22284883c82d8996fbade4fb629
Adam Chidlow <achidlow@users.noreply.github.com>
2023-03-14 14:30:10 +0800

    docs: refactor "golf" example to be exemplary for v1
    - import pyteal as pt

--- examples/golf/main.py
+++ examples/golf/main.py
@@ -1,29 +1,8 @@
 import base64
 import random
 
+import pyteal as pt
 from algosdk import v2client
-from pyteal import (
-    App,
-    Assert,
-    Bytes,
-    Concat,
-    Expr,
-    Extract,
-    ExtractUint64,
-    Global,
-    If,
-    Int,
-    Itob,
-    Log,
-    Or,
-    Return,
-    ScratchVar,
-    Seq,
-    Subroutine,
-    Suffix,
-    TealType,
-    abi,
-)
 
 from beaker import (
     Application,
@@ -38,8 +17,8 @@ from beaker.lib.math import Max
 
 class SortedIntegersState:
     elements = GlobalStateValue(
-        stack_type=TealType.uint64,
-        default=Int(0),
+        stack_type=pt.TealType.uint64,
+        default=pt.Int(0),
         descr="The number of elements in the array",
     )
 
@@ -48,9 +27,9 @@ _box_name = "sorted_ints"
 _box_size = 1024 * 4
 _max_ints = _box_size // 8
 
-BoxName = Bytes(_box_name)
-BoxSize = Int(_box_size)
-MaxInts = Int(_max_ints)
+BoxName = pt.Bytes(_box_name)
+BoxSize = pt.Int(_box_size)
+MaxInts = pt.Int(_max_ints)
 
 sorted_ints_app = Application(
     "SortedIntegers",
@@ -60,26 +39,28 @@ sorted_ints_app = Application(
 
 
 @sorted_ints_app.external
-def add_int(val: abi.Uint64, *, output: abi.DynamicArray[abi.Uint64]) -> Expr:
-    return Seq(
-        array_contents := App.box_get(BoxName),
-        Assert(Or(Int(0), Int(1))),
+def add_int(
+    val: pt.abi.Uint64, *, output: pt.abi.DynamicArray[pt.abi.Uint64]
+) -> pt.Expr:
+    return pt.Seq(
+        array_contents := pt.App.box_get(BoxName),
+        pt.Assert(pt.Or(pt.Int(0), pt.Int(1))),
         # figure out the correct index
         # Write the new array with the contents
-        (idx := ScratchVar()).store(
-            If(
-                sorted_ints_app.state.elements == Int(0),
-                Int(0),
+        (idx := pt.ScratchVar()).store(
+            pt.If(
+                sorted_ints_app.state.elements == pt.Int(0),
+                pt.Int(0),
                 binary_search(
                     val.get(),
                     array_contents.value(),
-                    Int(0),
-                    sorted_ints_app.state.elements - Int(1),
+                    pt.Int(0),
+                    sorted_ints_app.state.elements - pt.Int(1),
                 )
-                * Int(8),
+                * pt.Int(8),
             )
         ),
-        App.box_put(
+        pt.App.box_put(
             BoxName,
             # Take the bytes that would fit in the box
             insert_element(
@@ -89,20 +70,20 @@ def add_int(val: abi.Uint64, *, output: abi.DynamicArray[abi.Uint64]) -> Expr:
             ),
         ),
         sorted_ints_app.state.elements.increment(),
-        Log(Itob(Global.opcode_budget())),
+        pt.Log(pt.Itob(pt.Global.opcode_budget())),
         output.decode(
             # Prepend the bytes with the number of elements as a uint16,
             # according to ABI spec
-            Concat(
-                Suffix(Itob(Int(10)), Int(6)),
-                App.box_extract(BoxName, Int(0), Int(8) * Int(10)),
+            pt.Concat(
+                pt.Suffix(pt.Itob(pt.Int(10)), pt.Int(6)),
+                pt.App.box_extract(BoxName, pt.Int(0), pt.Int(8) * pt.Int(10)),
             )
         ),
     )
 
 
-@Subroutine(TealType.uint64)
-def binary_search(val: Expr, arr: Expr, start: Expr, end: Expr) -> Expr:
+@pt.Subroutine(pt.TealType.uint64)
+def binary_search(val: pt.Expr, arr: pt.Expr, start: pt.Expr, end: pt.Expr) -> pt.Expr:
     # Python equivalent:
     # def binary_search(arr, val, start, end):
     #     if start > end:
@@ -122,44 +103,46 @@ def binary_search(val: Expr, arr: Expr, start: Expr, end: Expr) -> Expr:
     #     else:
     #         return mid
 
-    return Seq(
-        If(start > end, Return(start)),
-        If(
+    return pt.Seq(
+        pt.If(start > end, pt.Return(start)),
+        pt.If(
             start == end,
-            Return(start + If(lookup_element(arr, start) > val, Int(0), Int(1))),
+            pt.Return(
+                start + pt.If(lookup_element(arr, start) > val, pt.Int(0), pt.Int(1))
+            ),
         ),
-        (mididx := ScratchVar()).store((start + end) / Int(2)),
-        (midval := ScratchVar()).store(lookup_element(arr, mididx.load())),
-        If(midval.load() < val)
+        (mididx := pt.ScratchVar()).store((start + end) / pt.Int(2)),
+        (midval := pt.ScratchVar()).store(lookup_element(arr, mididx.load())),
+        pt.If(midval.load() < val)
         .Then(
-            binary_search(val, arr, mididx.load() + Int(1), end),
+            binary_search(val, arr, mididx.load() + pt.Int(1), end),
         )
         .ElseIf(midval.load() > val)
         .Then(
-            binary_search(val, arr, start, Max(Int(1), mididx.load()) - Int(1)),
+            binary_search(val, arr, start, Max(pt.Int(1), mididx.load()) - pt.Int(1)),
         )
         .Else(mididx.load()),
     )
 
 
-def lookup_element(buff: Expr, idx: Expr) -> Expr:
-    return ExtractUint64(buff, idx * Int(8))
+def lookup_element(buff: pt.Expr, idx: pt.Expr) -> pt.Expr:
+    return pt.ExtractUint64(buff, idx * pt.Int(8))
 
 
-def insert_element(buff: Expr, new_val: Expr, pos: Expr) -> Expr:
-    return Concat(
-        Extract(buff, Int(0), pos),
+def insert_element(buff: pt.Expr, new_val: pt.Expr, pos: pt.Expr) -> pt.Expr:
+    return pt.Concat(
+        pt.Extract(buff, pt.Int(0), pos),
         new_val,
         # extract from pos -> max len of box leaving off
-        Extract(buff, pos, (BoxSize - pos) - Int(8)),
+        pt.Extract(buff, pos, (BoxSize - pos) - pt.Int(8)),
     )
 
 
 @sorted_ints_app.external
-def box_create_test() -> Expr:
-    return Seq(
-        Assert(App.box_create(BoxName, BoxSize)),
-        sorted_ints_app.state.elements.set(Int(0)),
+def box_create_test() -> pt.Expr:
+    return pt.Seq(
+        pt.Assert(pt.App.box_create(BoxName, BoxSize)),
+        sorted_ints_app.state.elements.set(pt.Int(0)),
     )
 
 
