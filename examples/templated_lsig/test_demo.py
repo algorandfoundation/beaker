9f022ad82abacbcca333646be56881595df15876
Adam Chidlow <achidlow@users.noreply.github.com>
2023-03-14 16:36:10 +0800

    docs: refactor "templated_lsig" example to be exemplary for v1
    - move demo code to its own file

--- examples/templated_lsig/test_demo.py
+++ examples/templated_lsig/test_demo.py
@@ -1,10 +1,11 @@
 from tests.conftest import check_application_artifacts_output_stability
 
-from examples.templated_lsig.main import app, demo
+from examples.templated_lsig import demo
+from examples.templated_lsig.main import app
 
 
 def test_demo() -> None:
-    demo()
+    demo.main()
 
 
 def test_output_stability() -> None:
