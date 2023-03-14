8beeef2c93b547e9c1dd49190b87f0ce0f899ba5
Adam Chidlow <achidlow@users.noreply.github.com>
2023-03-14 16:13:55 +0800

    docs: refactor "rsvp" example to be exemplary for v1
    - use recommended import structure for beaker, pyteal, and app(s)
    - use demo.py:main() vs demo.py:demo()
    - change misleading ABI method comment

--- examples/rsvp/contract.py
+++ examples/rsvp/contract.py
@@ -1,119 +1,99 @@
-from pyteal import (
-    Assert,
-    Balance,
-    Expr,
-    Global,
-    If,
-    InnerTxnBuilder,
-    Int,
-    Seq,
-    Subroutine,
-    TealType,
-    Txn,
-    TxnField,
-    TxnType,
-    abi,
-)
+import pyteal as pt
 
-from beaker import (
-    Application,
-    Authorize,
-    GlobalStateValue,
-    LocalStateValue,
-)
+import beaker
 
 ############
 # Constants#
 ############
 
 # Contract address minimum balance
-MIN_BAL = Int(100000)
+MIN_BAL = pt.Int(100000)
 
 # Algorand minimum txn fee
-FEE = Int(1000)
+FEE = pt.Int(1000)
 
 
-@Subroutine(TealType.none)
-def withdraw_funds() -> Expr:
+@pt.Subroutine(pt.TealType.none)
+def withdraw_funds() -> pt.Expr:
     """Helper method that withdraws funds in the RSVP contract"""
-    rsvp_bal = Balance(Global.current_application_address())
-    return Seq(
-        Assert(
+    rsvp_bal = pt.Balance(pt.Global.current_application_address())
+    return pt.Seq(
+        pt.Assert(
             rsvp_bal > (MIN_BAL + FEE),
         ),
-        InnerTxnBuilder.Execute(
+        pt.InnerTxnBuilder.Execute(
             {
-                TxnField.type_enum: TxnType.Payment,
-                TxnField.receiver: Txn.sender(),
-                TxnField.amount: rsvp_bal - (MIN_BAL + FEE),
+                pt.TxnField.type_enum: pt.TxnType.Payment,
+                pt.TxnField.receiver: pt.Txn.sender(),
+                pt.TxnField.amount: rsvp_bal - (MIN_BAL + FEE),
             }
         ),
     )
 
 
 class EventRSVPState:
-    price = GlobalStateValue(
-        stack_type=TealType.uint64,
-        default=Int(1_000_000),
+    price = beaker.GlobalStateValue(
+        stack_type=pt.TealType.uint64,
+        default=pt.Int(1_000_000),
         descr="The price of the event. Default price is 1 Algo",
     )
 
-    rsvp = GlobalStateValue(
-        stack_type=TealType.uint64,
-        default=Int(0),
+    rsvp = beaker.GlobalStateValue(
+        stack_type=pt.TealType.uint64,
+        default=pt.Int(0),
         descr="Number of people who RSVPed to the event",
     )
 
-    checked_in = LocalStateValue(
-        stack_type=TealType.uint64,
-        default=Int(0),
+    checked_in = beaker.LocalStateValue(
+        stack_type=pt.TealType.uint64,
+        default=pt.Int(0),
         descr="0 = not checked in, 1 = checked in",
     )
 
 
-rsvp = Application("EventRSVP", state=EventRSVPState())
+app = beaker.Application("EventRSVP", state=EventRSVPState())
 
 
-@rsvp.create
-def create(event_price: abi.Uint64) -> Expr:
+@app.create
+def create(event_price: pt.abi.Uint64) -> pt.Expr:
     """Deploys the contract and initialze the app states"""
-    return Seq(
-        rsvp.initialize_global_state(),
-        rsvp.state.price.set(event_price.get()),
+    return pt.Seq(
+        app.initialize_global_state(),
+        app.state.price.set(event_price.get()),
     )
 
 
-@rsvp.opt_in
-def do_rsvp(payment: abi.PaymentTransaction) -> Expr:
+@app.opt_in
+def do_rsvp(payment: pt.abi.PaymentTransaction) -> pt.Expr:
     """Let txn sender rsvp to the event by opting into the contract"""
-    return Seq(
-        Assert(
-            Global.group_size() == Int(2),
-            payment.get().receiver() == Global.current_application_address(),
-            payment.get().amount() == rsvp.state.price,
+    return pt.Seq(
+        pt.Assert(
+            pt.Global.group_size() == pt.Int(2),
+            payment.get().receiver() == pt.Global.current_application_address(),
+            payment.get().amount() == app.state.price,
         ),
-        rsvp.initialize_local_state(),
-        rsvp.state.rsvp.increment(),
+        app.initialize_local_state(),
+        app.state.rsvp.increment(),
     )
 
 
-@rsvp.external(authorize=Authorize.opted_in(Global.current_application_id()))
-def check_in() -> Expr:
+@app.external(authorize=beaker.Authorize.opted_in())
+def check_in() -> pt.Expr:
     """If the Sender RSVPed, check-in the Sender"""
-    return rsvp.state.checked_in.set(Int(1))
+    return app.state.checked_in.set(pt.Int(1))
 
 
-@rsvp.external(authorize=Authorize.only_creator())
-def withdraw_external() -> Expr:
+@app.external(authorize=beaker.Authorize.only_creator())
+def withdraw_external() -> pt.Expr:
     """Let event creator to withdraw all funds in the contract"""
     return withdraw_funds()
 
 
-@rsvp.delete(bare=True, authorize=Authorize.only_creator())
-def delete() -> Expr:
+@app.delete(bare=True, authorize=beaker.Authorize.only_creator())
+def delete() -> pt.Expr:
     """Let event creator delete the contract. Withdraws remaining funds"""
-    return If(
-        Balance(Global.current_application_address()) > (MIN_BAL + FEE),
+    return pt.If(
+        pt.Balance(pt.Global.current_application_address()) > (MIN_BAL + FEE),
         withdraw_funds(),
     )
 
@@ -123,39 +103,39 @@ def delete() -> Expr:
 ################
 
 
-@rsvp.external(read_only=True, authorize=Authorize.only_creator())
-def read_rsvp(*, output: abi.Uint64) -> Expr:
+@app.external(read_only=True, authorize=beaker.Authorize.only_creator())
+def read_rsvp(*, output: pt.abi.Uint64) -> pt.Expr:
     """Read amount of RSVP to the event. Only callable by Creator."""
-    return output.set(rsvp.state.rsvp)
+    return output.set(app.state.rsvp)
 
 
-@rsvp.external(read_only=True)
-def read_price(*, output: abi.Uint64) -> Expr:
-    """Read amount of RSVP to the event. Only callable by Creator."""
-    return output.set(rsvp.state.price)
+@app.external(read_only=True)
+def read_price(*, output: pt.abi.Uint64) -> pt.Expr:
+    """Read amount of RSVP to the event."""
+    return output.set(app.state.price)
 
 
-def _do_refund() -> Expr:
+def _do_refund() -> pt.Expr:
     """Refunds event payment to guests"""
-    return Seq(
-        InnerTxnBuilder.Begin(),
-        InnerTxnBuilder.SetFields(
+    return pt.Seq(
+        pt.InnerTxnBuilder.Begin(),
+        pt.InnerTxnBuilder.SetFields(
             {
-                TxnField.type_enum: TxnType.Payment,
-                TxnField.receiver: Txn.sender(),
-                TxnField.amount: rsvp.state.price - FEE,
+                pt.TxnField.type_enum: pt.TxnType.Payment,
+                pt.TxnField.receiver: pt.Txn.sender(),
+                pt.TxnField.amount: app.state.price - FEE,
             }
         ),
-        InnerTxnBuilder.Submit(),
-        rsvp.state.rsvp.decrement(),
+        pt.InnerTxnBuilder.Submit(),
+        app.state.rsvp.decrement(),
     )
 
 
-@rsvp.close_out(bare=True, name="refund")
-def close_out() -> Expr:
+@app.close_out(bare=True, name="refund")
+def refund() -> pt.Expr:
     return _do_refund()
 
 
-@rsvp.clear_state(name="refund")
-def clear_state() -> Expr:
+@app.clear_state(name="refund")
+def clear_state() -> pt.Expr:
     return _do_refund()
