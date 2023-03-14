832e5c1c3faea9e9ff5e4aab7a1a8c6bdec06fec
Adam Chidlow <achidlow@users.noreply.github.com>
2023-03-14 15:23:52 +0800

    docs: refactor "offload_compute" example to be exemplary for v1
    - separate demo code from app/lsig code
    - use recommended import structure for beaker, pyteal, and app(s)
    - use demo.py:main() vs main.py:demo()

--- examples/offload_compute/test_demo.py
+++ examples/offload_compute/test_demo.py
@@ -1,11 +1,13 @@
 from tests.conftest import check_application_artifacts_output_stability
 
-from examples.offload_compute.main import demo, eth_checker
+from examples.offload_compute import demo, eth_checker
 
 
 def test_demo() -> None:
-    demo()
+    demo.main()
 
 
 def test_output_stability() -> None:
-    check_application_artifacts_output_stability(eth_checker, dir_per_test_file=False)
+    check_application_artifacts_output_stability(
+        eth_checker.app, dir_per_test_file=False
+    )
