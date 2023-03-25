53f43acd82b59fd381fdf4df7bdea8d065594d67
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-03-25 09:22:31 -0400

    add working version

--- examples/box-expr/app.py
+++ examples/box-expr/app.py
@@ -30,11 +30,19 @@ def makeit() -> pt.Expr:
 def doit() -> pt.Expr:
     current_vote_tally = pt.Btoi(app.state.m[pt.Txn.sender()].get())
     new_vote_tally = pt.Add(current_vote_tally, pt.Int(1))
-    # new_vote_tally is an Expr with deferred evaluation of the value
-    # on `set` the mapping object will first Delete the box then try to BoxPut
-    # but BoxPut will fail since new_vote_tally Expression tries to _get_ the value from
+    # new_vote_tally is an Expr with deferred evaluation of the value,
+    # on `set` the mapping object will first BoxDelete then try to BoxPut
+    # but BoxPut will fail since new_vote_tally Expr tries to BoxGet the value from
     # the box that was just deleted.
     return app.state.m[pt.Txn.sender()].set(pt.Itob(new_vote_tally))
+
+
+@app.external
+def doit_different() -> pt.Expr:
+    return pt.Seq(
+        app.state.m[pt.Txn.sender()].store_into(current_vote_tally := pt.abi.Uint64()),
+        app.state.m[pt.Txn.sender()].set(pt.Itob(current_vote_tally.get() + pt.Int(1))),
+    )
 
 
 if __name__ == "__main__":
@@ -48,4 +56,9 @@ if __name__ == "__main__":
     app_client.create()
     app_client.fund(int(1e7))
     app_client.call(makeit, boxes=[(0, decode_address(acct.address))])
-    app_client.call(doit, boxes=[(0, decode_address(acct.address))])
+    try:
+        app_client.call(doit, boxes=[(0, decode_address(acct.address))])
+    except Exception as e:
+        print(e)
+
+    app_client.call(doit_different, boxes=[(0, decode_address(acct.address))])
