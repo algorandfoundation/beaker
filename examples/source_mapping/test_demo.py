18203be62dc6ad2fb45689ea85294f1154543bc1
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-03-30 14:03:54 -0400

    remove tests from sourcemap example to make it faster

--- examples/source_mapping/test_demo.py
+++ examples/source_mapping/test_demo.py
@@ -1,13 +1,14 @@
-from tests.conftest import check_application_artifacts_output_stability
-
-from examples.source_mapping import app, demo
-
-
-def test_demo() -> None:
-    demo.main()
-
-
-def test_output_stability() -> None:
-    check_application_artifacts_output_stability(
-        app=app.source_mapped_app, dir_per_test_file=False
-    )
+# from tests.conftest import check_application_artifacts_output_stability
+#
+# from examples.source_mapping import app, demo
+#
+#
+# def test_demo() -> None:
+#    demo.main()
+#
+#
+# def test_output_stability() -> None:
+#    check_application_artifacts_output_stability(
+#        app=app.source_mapped_app, dir_per_test_file=False
+#    )
+#
