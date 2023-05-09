8bcb8592bcb333f9b831dea1f23f9ebd012cdd69
Joe Polny <joepolny@gmail.com>
2023-05-09 16:38:24 -0400

    populate __all__ to appease both mypy and ruff

---
+++ beaker/sandbox/clients.py
@@ -0,0 +1,17 @@
+from ..localnet.clients import (
+    DEFAULT_ALGOD_ADDRESS,
+    DEFAULT_ALGOD_TOKEN,
+    DEFAULT_INDEXER_ADDRESS,
+    DEFAULT_INDEXER_TOKEN,
+    get_algod_client,
+    get_indexer_client,
+)
+
+__all__ = [
+    "get_algod_client",
+    "get_indexer_client",
+    "DEFAULT_ALGOD_ADDRESS",
+    "DEFAULT_ALGOD_TOKEN",
+    "DEFAULT_INDEXER_ADDRESS",
+    "DEFAULT_INDEXER_TOKEN",
+]
--- beaker/sandbox/kmd.py
+++ beaker/sandbox/kmd.py
@@ -1,6 +1,15 @@
 from ..localnet.kmd import (
+    DEFAULT_KMD_ADDRESS,
+    DEFAULT_KMD_TOKEN,
+    DEFAULT_KMD_WALLET_NAME,
+    DEFAULT_KMD_WALLET_PASSWORD,
     LocalAccount,
+    add_account,
+    delete_account,
+    get_accounts,
+    get_client,
     get_localnet_default_wallet,
+    wallet_handle_by_name,
 )
 
 get_sandbox_default_wallet = get_localnet_default_wallet
@@ -8,3 +17,17 @@ get_sandbox_default_wallet = get_localnet_default_wallet
 
 class SandboxAccount(LocalAccount):
     pass
+
+__all__ = [
+    "get_client",
+    "get_sandbox_default_wallet",
+    "SandboxAccount",
+    "get_accounts",
+    "add_account",
+    "delete_account",
+    "wallet_handle_by_name",
+    "DEFAULT_KMD_ADDRESS",
+    "DEFAULT_KMD_TOKEN",
+    "DEFAULT_KMD_WALLET_NAME",
+    "DEFAULT_KMD_WALLET_PASSWORD",
+]
