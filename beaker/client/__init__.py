2b463cb8991ab87355c435bc21da6b2b8aa863fe
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2023-10-09 10:37:50 +0200

    feat: bumping algokit utils, pyteal; removing algoexplorer as api provider

--- beaker/client/__init__.py
+++ beaker/client/__init__.py
@@ -1,11 +1,10 @@
 from algokit_utils import LogicError
 
-from .api_providers import AlgoExplorer, AlgoNode, Network, PureStake, Sandbox
+from .api_providers import AlgoNode, Network, PureStake, Sandbox
 from .application_client import ApplicationClient
 
 LogicException = LogicError
 __all__ = [
-    "AlgoExplorer",
     "AlgoNode",
     "ApplicationClient",
     "LogicException",
